6c7a5297fd1a953d9d905e80e2a09c33ebc9fad6
ErandIsufi <162713206+ErandIsufi@users.noreply.github.com>
2025-04-03 15:10:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PINGPONG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player1 = new System.Windows.Forms.GroupBox();
+            this.Player2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Ball = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.movementTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Player1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Player1.Location = new System.Drawing.Point(767, 116);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(21, 94);
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            this.Player1.Text = "groupBox1";
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Player2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Player2.Location = new System.Drawing.Point(12, 116);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(21, 94);
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
+            this.Player2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox3.Location = new System.Drawing.Point(399, 1);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(2, 378);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ball.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ball.Location = new System.Drawing.Point(391, 157);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.TabIndex = 4;
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Font = new System.Drawing.Font("Proxy 6", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2Score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPlayer2Score.Location = new System.Drawing.Point(300, 276);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(72, 78);
+            this.lblPlayer2Score.TabIndex = 5;
+            this.lblPlayer2Score.Text = "0";
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Font = new System.Drawing.Font("Proxy 5", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPlayer1Score.Location = new System.Drawing.Point(429, 276);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(72, 78);
+            this.lblPlayer1Score.TabIndex = 6;
+            this.lblPlayer1Score.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 365);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Player1;
+        private System.Windows.Forms.GroupBox Player2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Panel Ball;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer movementTimer;
+        private System.Windows.Forms.Label lblPlayer2Score;
+        private System.Windows.Forms.Label lblPlayer1Score;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PINGPONG
+{
+    public partial class Form1 : Form
+    {
+        private int ballSpeedX = 7; // Sa shpejt esht topi nalt kur shkon
+        private int ballSpeedY = 7; // Topi Sa shpejt esht posht kur shkon
+        private Timer Pimer;
+        private bool isMovingUp = false;
+        private bool isMovingDown = false;
+        private Timer movementTimerr;
+
+        private Timer aiMovementTimer;
+        private int aiSpeed = 6;
+
+
+        private int player1Score = 0;
+        private int player2Score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            MakePanelCircular();
+            InitializeTimer();
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            this.KeyUp += Form1_KeyUp;
+
+
+            movementTimer = new Timer();
+            movementTimer.Interval = 10; 
+            movementTimer.Tick += MovementTimer_Tick;
+            movementTimer.Start();
+
+            //Kjo esht per kompjuterin
+            aiMovementTimer = new Timer();
+            aiMovementTimer.Interval = 10; 
+            aiMovementTimer.Tick += AIMovementTimer_Tick;
+            aiMovementTimer.Start();
+
+            lblPlayer1Score.Text = "0";
+            lblPlayer2Score.Text = "0";
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                isMovingUp = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                isMovingDown = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                isMovingUp = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                isMovingDown = false;
+            }
+        }
+        private void MovementTimer_Tick(object sender, EventArgs e)
+        {
+            int step = 5; 
+
+            if (isMovingUp && Player1.Top > 0)
+            {
+                Player1.Top -= step;
+            }
+
+            if (isMovingDown && Player1.Bottom < this.ClientSize.Height)
+            {
+                Player1.Top += step;
+            }
+        }
+
+        private void AIMovementTimer_Tick(object sender, EventArgs e)
+        {
+            // Qe me livrit kompjuteri veq kur esht topi ngat komputerit
+            if (Player2.Top < Ball.Top - 50 || Player2.Bottom > Ball.Bottom + 50)
+            {
+                if (Ball.Top + Ball.Height / 2 < Player2.Top + Player2.Height / 2)
+                {
+                    // Me livrit nalt nese o ti shku topi nalt
+                    if (Player2.Top > 0)
+                    {
+                        Player2.Top -= aiSpeed;
+                    }
+                }
+                else if (Ball.Top + Ball.Height / 2 > Player2.Top + Player2.Height / 2)
+                {
+                    // me livrit posht nese o ti shku topi posht
+                    if (Player2.Bottom < this.ClientSize.Height)
+                    {
+                        Player2.Top += aiSpeed;
+                    }
+                }
+            }
+        }
+
+        private void MakePanelCircular()
+        {
+            
+            if (Ball.Width == Ball.Height)
+            {
+                GraphicsPath path = new GraphicsPath();
+                path.AddEllipse(0, 0, Ball.Width, Ball.Height);
+                Ball.Region = new Region(path);
+            }
+            else
+            {
+                MessageBox.Show("Make sure the Ball panel's width and height are equal!");
+            }
+        }
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 10; // 10 milliseconds
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Topi me livirt
+            Ball.Left += ballSpeedX;
+            Ball.Top += ballSpeedY;
+
+            // Kur ti rrok muret
+            if (Ball.Top <= 0 || Ball.Bottom >= this.ClientSize.Height)
+            {
+                ballSpeedY = -ballSpeedY; 
+            }
+
+            // Kur te kalon topi murin e majt ose te djath per me i shtu pikt edhe me resetu lojen
+            if (Ball.Left <= 0)
+            {
+                player2Score++; // Kur te jep aj muti
+                UpdateScoreDisplay();
+                ResetBall();
+            }
+            else if (Ball.Right >= this.ClientSize.Width)
+            {
+                player1Score++; // Kur te jepsh ti goll
+                UpdateScoreDisplay();
+                ResetBall();
+            }
+
+            // Kodi per me dedektu kur rroket topi me lojtar
+            if (Ball.Bounds.IntersectsWith(Player1.Bounds) || Ball.Bounds.IntersectsWith(Player2.Bounds))
+            {
+                ballSpeedX = -ballSpeedX; // qe  me ja ndrru drejtimin topti
+            }
+        }
+
+        private void UpdateScoreDisplay()
+        {
+            lblPlayer2Score.Text =  player1Score.ToString();
+            lblPlayer1Score.Text =  player2Score.ToString();
+        }
+        private void ResetBall()
+        {
+            // qe me qit topin ne qender apet
+            Ball.Left = this.ClientSize.Width / 2 - Ball.Width / 2;
+            Ball.Top = this.ClientSize.Height / 2 - Ball.Height / 2;
+
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
